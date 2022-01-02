726d62fc8b92b90388179f65c934d7a57fa95fcf
Joel Paucarima Franco <jopau3112@gmail.com>
2022-01-02 15:23:37 +0100

    Change toggle theme in header

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,38 +1,59 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import Button from '../../UI/Button/Button';
-import { getThemeMode } from '../../../utils/helpers';
+import { BiSun, BiMoon } from 'react-icons/bi';
+import { THEMES } from '../../../utils/constants';
 import styles from './Header.module.css';
 
 const Header = ({ title, logoUrl, theme, onChangeTheme }) => {
-  return theme && (
-    <div className={styles.wrapper}>
-      <Link to="/" className={styles.brand}>
-        <img
-          className={styles.logo}
-          src={logoUrl || `/assets/images/logo-${theme}.png`}
-          alt="logo"></img>
-        <p className={styles.title}>{title}</p>
-      </Link>
+  const ToggleTheme = () => {
+    return (
+      <label>
+        <input
+          className={styles['toggle-checkbox']}
+          type="checkbox"
+          checked={theme === THEMES.DARK}
+          onChange={onChangeTheme}></input>
+        <div className={styles['toggle-slot']}>
+          <div className={styles['sun-icon-wrapper']}>
+            <BiSun className={styles['sun-icon']} />
+          </div>
+          <div className={styles['toggle-button']}></div>
+          <div className={styles['moon-icon-wrapper']}>
+            <BiMoon className={styles['moon-icon']} />
+          </div>
+        </div>
+      </label>
+    );
+  };
 
-      <div className={styles.actions}>
-        <Button clear size="small" className={styles.theme} onClick={onChangeTheme}>
-          <img src={`/assets/icons/icon-${getThemeMode(theme)}.svg`}></img>
-          {theme.toUpperCase()}
-        </Button>
+  return (
+    theme && (
+      <div className={styles.wrapper}>
+        <Link to="/" className={styles.brand}>
+          <img
+            className={styles.logo}
+            src={logoUrl || `/assets/images/logo-${theme}.png`}
+            alt="logo"></img>
+          <p className={styles.title}>{title}</p>
+        </Link>
+
+        <div className={styles.actions}>
+          <div className={styles['theme-action']}>
+            <span>{theme.toUpperCase()}</span>
+            {ToggleTheme(onChangeTheme)}
+          </div>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
 Header.defaultProps = {
   title: 'Title',
-  theme: '',
-  logoUrl: ''
 };
 Header.propTypes = {
   title: PropTypes.string.isRequired,
-  logoUrl: PropTypes.string.isRequired,
+  logoUrl: PropTypes.string,
   theme: PropTypes.oneOf(['light', 'dark', false]).isRequired,
   onChangeTheme: PropTypes.func
 };
