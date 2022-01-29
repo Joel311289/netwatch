2363f883de87043b0654f2dcad1048b6bc241b40
Joel Paucarima Franco <jopau3112@gmail.com>
2022-01-29 01:46:29 +0100

    Fix load first theme not saved

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,19 +1,24 @@
 import { createContext, useState } from 'react';
 
 import { ContextPropTypes } from '@utils/constants/proptypes';
-import { THEME_STORAGE_KEY } from '@utils/constants';
+import { THEMES, THEME_STORAGE_KEY } from '@utils/constants';
 import { getThemeMode } from '@utils/helpers';
 
 export const ThemeContext = createContext({});
 
 export const ThemeContextProvider = ({ children }) => {
-  const [value, setValue] = useState(() => window.localStorage.getItem(THEME_STORAGE_KEY));
+  const [value, setValue] = useState(() => {
+    const valueStoraged = window.localStorage.getItem(THEME_STORAGE_KEY);
+    if (valueStoraged) return valueStoraged;
+    window.localStorage.setItem(THEME_STORAGE_KEY, THEMES.LIGHT);
+    return THEMES.LIGHT;
+  });
 
   const theme = {
     theme: value,
     setTheme: (current) => {
       setValue(getThemeMode(current));
-      window.localStorage.setItem(THEME_STORAGE_KEY, getThemeMode(value));
+      window.localStorage.setItem(THEME_STORAGE_KEY, getThemeMode(current));
     }
   };
 
