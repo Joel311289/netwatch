e7ddf5d5a17112a43bd086fcd792890a86510cab
Joel Paucarima <jopau3112@gmail.com>
2022-02-12 19:42:30 +0100

    Refactor config section detail pages

--- src/pages/MediaDetailSection/MediaDetailSectionPage.jsx
+++ src/pages/MediaDetailSection/MediaDetailSectionPage.jsx
@@ -1,4 +1,3 @@
-import { useFetch } from '@hooks/useFetch';
 import { useMediaPath } from '@hooks/useMediaPath';
 import { useServiceMediaDetail } from '@hooks/useServiceMediaDetail';
 import { useBreakpointViewport } from '@hooks/useBreakpointViewport';
@@ -13,42 +12,23 @@ import { routeMediaDetail } from '@services/helpers';
 import { string } from '@utils/helpers/strings';
 import { isEmptyArray } from '@utils/helpers/arrays';
 
-import {
-  apiSectionDetail,
-  fetcherSectionDetail,
-  resumeProps,
-  sectionProps
-} from '@pages/MediaDetailSection/config';
-import { Selector } from '@pages/MediaDetailSection/components';
+import { sectionProps } from '@pages/MediaDetailSection/config';
 import styles from '@pages/MediaDetailSection/MediaDetailSectionPage.module.css';
 
 const MediaDetailSectionPage = () => {
   const breakpoint = useBreakpointViewport();
-  const { mediaType, id, section, sectionId, params } = useMediaPath([
-    '/:mediaType/:key/:section',
-    '/:mediaType/:key/:section/:keySection',
-    '/:mediaType/:key/:section/:keySection/:section/:keySection'
-  ]);
-
+  const { mediaType, id, section } = useMediaPath(['/:mediaType/:key/:section']);
   const { data, loading } = useServiceMediaDetail(mediaType, id, [section]);
-  const { data: detailSection, loading: loadingSection } = useFetch(
-    sectionId ? apiSectionDetail(id, params)[section] : null,
-    fetcherSectionDetail[section]
-  );
 
   const { title, [section]: detail } = data;
-  const resume = resumeProps({ data, detail, detailSection })[section] || {};
-  const { label, length, sections } = sectionProps({ data, detail, detailSection })[section] || {};
-
-  const SectionSelector = Selector({ data, detail, detailSection }, () => {})[section];
-
-  console.log(data, detailSection, section);
+  const { label, length, sections } = sectionProps(data, section);
 
   return (
     <Space
       nowrap
       direction="column"
-      className={`${styles.wrapper} ${string(styles[section.replace(/\//g, '_')])}`}>
+      className={`${styles.wrapper} ${string(styles[section.replace(/\//g, '_')])}`}
+    >
       <div className={`${styles.resume} theme-dark`}>
         <MediaResume
           skeleton={loading}
@@ -57,7 +37,6 @@ const MediaDetailSectionPage = () => {
           title={title}
           linkName="Volver a principal"
           {...data}
-          {...resume}
         />
       </div>
 
@@ -68,41 +47,37 @@ const MediaDetailSectionPage = () => {
           </h2>
         )}
 
-        {SectionSelector && SectionSelector()}
+        {detail &&
+          (sections || []).map(
+            ({ heading, gridProps, Element, items, props, emptyMessage }, index) =>
+              (!isEmptyArray(items) || emptyMessage) && (
+                <div className={styles.subsection} key={index}>
+                  {heading && (
+                    <div className="sub-heading">
+                      <MediaHeading text={heading} />
+                    </div>
+                  )}
 
-        <Space direction="column" gap={50} nowrap>
-          {(detail || detailSection) &&
-            (sections || []).map(
-              ({ heading, gridProps, Element, items, props, emptyMessage }, index) =>
-                (!isEmptyArray(items) || emptyMessage) && (
-                  <div className={styles.subsection} key={index}>
-                    {heading && (
-                      <div className="sub-heading">
-                        <MediaHeading text={heading} />
-                      </div>
-                    )}
+                  {Array.isArray(items) && (
+                    <Grid {...gridProps(breakpoint)}>
+                      {items.map((item, index) => (
+                        <Element
+                          key={index}
+                          skeleton={loading}
+                          route={routeMediaDetail(data)}
+                          {...item}
+                          {...props(item)}
+                        />
+                      ))}
+                    </Grid>
+                  )}
 
-                    {Array.isArray(items) && (
-                      <Grid {...gridProps(breakpoint)}>
-                        {items.map((item, index) => (
-                          <Element
-                            key={index}
-                            skeleton={!sectionId ? loading : loadingSection}
-                            route={routeMediaDetail(data)}
-                            {...item}
-                            {...props(item)}
-                          />
-                        ))}
-                      </Grid>
-                    )}
-
-                    {Array.isArray(items) && !items.length && emptyMessage && (
-                      <div className={styles.empty}>{emptyMessage}</div>
-                    )}
-                  </div>
-                )
-            )}
-        </Space>
+                  {Array.isArray(items) && !items.length && emptyMessage && (
+                    <div className={styles.empty}>{emptyMessage}</div>
+                  )}
+                </div>
+              )
+          )}
       </div>
     </Space>
   );
--- src/pages/MediaDetailSection/config.js
+++ src/pages/MediaDetailSection/config.js
@@ -4,12 +4,6 @@ import MediaItemImage from '@components/Media/MediaItem/MediaItem-image';
 import MediaItem from '@components/Media/MediaItem/MediaItem';
 import MediaItemCredit from '@components/Media/MediaItem/MediaItem-credit';
 import MediaItemSeason from '@components/Media/MediaItem/MediaItem-season';
-import MediaItemEpisode from '@components/Media/MediaItem/MediaItem-episode';
-
-import { mediaTypes } from '@services/constants';
-import { routeMediaDetail } from '@services/helpers';
-import { getSeasonSerie } from '@services/series/get-season';
-import { getSeasonEpisodeSerie } from '@services/series/get-episode';
 
 const itemsPerRow = (breakpoint, { mobile, tablet, smallDesktop, defaultValue = 3 }) => {
   if (breakpoint.mobile) return mobile;
@@ -40,167 +34,91 @@ const labelCredits = {
   writers: 'Escritores'
 };
 
-export const apiSectionDetail = (mediaId, params) => ({
-  'seasons/detail': `/api/${mediaTypes.TV}/${mediaId}/season/${get(params, 'number_season')}`,
-  'episodes/detail': `/api/${mediaTypes.TV}/${mediaId}/season/${get(
-    params,
-    'number_season'
-  )}/episode/${get(params, 'number_episode')}`
-});
-export const fetcherSectionDetail = {
-  'seasons/detail': getSeasonSerie,
-  'episodes/detail': getSeasonEpisodeSerie
+export const sectionProps = (data, section) => {
+  const title = get(data, 'title');
+  const number_seasons = get(data, 'number_seasons');
+  const detail = get(data, section);
+
+  return (
+    {
+      videos: {
+        label: 'Vídeos',
+        length: (detail || []).length,
+        sections: [
+          {
+            emptyMessage: `No se han encontrado vídeos para ${title}`,
+            gridProps: (breakpoint) => ({
+              gap: '0 10px',
+              itemsPerRow: itemsPerRowBackdrop(breakpoint)
+            }),
+            Element: MediaItem,
+            items: detail,
+            props: ({ site, key, name }) => ({
+              title: name,
+              videoKey: key,
+              videoSite: site
+            })
+          }
+        ]
+      },
+      images: {
+        label: 'Imágenes',
+        sections: Object.keys(detail || {})
+          .filter((key) => labelImages[key])
+          .map((key) => ({
+            emptyMessage: `No se han encontrado ${(
+              labelImages[key] || ''
+            ).toLowerCase()} para ${get(data, 'title')}`,
+            gridProps: (breakpoint) => ({
+              gap: '20px 10px',
+              itemsPerRow:
+                key === 'backdrops'
+                  ? itemsPerRowBackdrop(breakpoint)
+                  : itemsPerRowPoster(breakpoint)
+            }),
+            heading: `${labelImages[key]} (${detail[key].length})`,
+            Element: MediaItemImage,
+            items: detail[key],
+            props: (item) => ({
+              ...item,
+              ratio: Math.max(...detail[key].map(({ ratio }) => ratio)),
+              zoom: true
+            })
+          }))
+      },
+      seasons: {
+        label: 'Temporadas',
+        length: number_seasons,
+        sections: [
+          {
+            emptyMessage: `No se han encontrado temporadas para ${title}`,
+            gridProps: () => ({
+              gap: '20px',
+              itemsPerRow: 1
+            }),
+            Element: MediaItemSeason,
+            items: detail,
+            props: (props) => ({ ...props })
+          }
+        ]
+      },
+      credits: {
+        label: 'Reparto',
+        sections: Object.keys(detail || {}).map((key) => ({
+          emptyMessage: `No se han encontrado ${(labelCredits[key] || '').toLowerCase()} para ${get(
+            data,
+            'title'
+          )}`,
+          gridProps: (breakpoint) => ({
+            gap: '20px 10px',
+            itemsPerRow: itemsPerRowCredit(breakpoint)
+          }),
+          heading: labelCredits[key],
+          Element: MediaItemCredit,
+          items: detail[key],
+          props: (item) => ({ ...item })
+        }))
+      }
+    }[section] || {}
+  );
 };
-
-export const resumeProps = ({ data, detailSection }) => ({
-  'seasons/detail': {
-    to: `${routeMediaDetail(data)}/seasons`,
-    seasons: get(data, 'seasons'),
-    numberSeasonActive: get(detailSection, 'season_number'),
-    linkName: 'Volver a temporadas',
-    date: get(detailSection, 'date'),
-    image: get(detailSection, 'image')
-  }
-});
-
-export const sectionProps = ({ data, detail, detailSection }) => ({
-  videos: {
-    label: 'Vídeos',
-    length: (detail || []).length,
-    sections: [
-      {
-        emptyMessage: `No se han encontrado vídeos para ${get(data, 'title')}`,
-        gridProps: (breakpoint) => ({
-          gap: '0 10px',
-          itemsPerRow: itemsPerRowBackdrop(breakpoint)
-        }),
-        Element: MediaItem,
-        items: detail,
-        props: ({ site, key, name }) => ({
-          title: name,
-          videoKey: key,
-          videoSite: site
-        })
-      }
-    ]
-  },
-  images: {
-    label: 'Imágenes',
-    sections: Object.keys(detail || {})
-      .filter((key) => labelImages[key])
-      .map((key) => ({
-        emptyMessage: `No se han encontrado ${(labelImages[key] || '').toLowerCase()} para ${get(
-          data,
-          'title'
-        )}`,
-        gridProps: (breakpoint) => ({
-          gap: '20px 10px',
-          itemsPerRow:
-            key === 'backdrops' ? itemsPerRowBackdrop(breakpoint) : itemsPerRowPoster(breakpoint)
-        }),
-        heading: `${labelImages[key]} (${detail[key].length})`,
-        Element: MediaItemImage,
-        items: detail[key],
-        props: (item) => ({
-          ...item,
-          ratio: Math.max(...detail[key].map(({ ratio }) => ratio)),
-          zoom: true
-        })
-      }))
-  },
-  seasons: {
-    label: 'Temporadas',
-    length: get(data, 'number_seasons'),
-    sections: [
-      {
-        emptyMessage: `No se han encontrado temporadas para ${get(data, 'title')}`,
-        gridProps: () => ({
-          gap: '20px',
-          itemsPerRow: 1
-        }),
-        Element: MediaItemSeason,
-        items: detail,
-        props: (props) => ({ ...props })
-      }
-    ]
-  },
-  credits: {
-    label: 'Reparto',
-    sections: Object.keys(detail || {}).map((key) => ({
-      emptyMessage: `No se han encontrado ${(labelCredits[key] || '').toLowerCase()} para ${get(
-        data,
-        'title'
-      )}`,
-      gridProps: (breakpoint) => ({
-        gap: '20px 10px',
-        itemsPerRow: itemsPerRowCredit(breakpoint)
-      }),
-      heading: labelCredits[key],
-      Element: MediaItemCredit,
-      items: detail[key],
-      props: (item) => ({ ...item })
-    }))
-  },
-  'seasons/detail': {
-    // label: `${get(detailSection, 'title')}`,
-    // length: get(detailSection, 'episodes', []).length,
-    sections: [
-      {
-        heading: 'Episodios',
-        emptyMessage: `No se han encontrado episodios para "${get(
-          detailSection,
-          'title'
-        )}" de ${get(data, 'title')}`,
-        gridProps: () => ({
-          gap: '20px',
-          itemsPerRow: 1
-        }),
-        Element: MediaItemEpisode,
-        items: get(detailSection, 'episodes', []),
-        props: (props) => ({ ...props })
-      },
-      {
-        heading: 'Vídeos',
-        gridProps: (breakpoint) => ({
-          gap: '0 10px',
-          itemsPerRow: itemsPerRowBackdrop(breakpoint)
-        }),
-        Element: MediaItem,
-        items: get(detailSection, 'videos', null),
-        props: ({ site, key, name }) => ({
-          title: name,
-          videoKey: key,
-          videoSite: site
-        })
-      },
-      {
-        heading: labelImages.backdrops,
-        gridProps: (breakpoint) => ({
-          gap: '20px 10px',
-          itemsPerRow: itemsPerRowBackdrop(breakpoint)
-        }),
-        Element: MediaItemImage,
-        items: get(detailSection, 'images.backdrops', []),
-        props: (item) => ({
-          ...item,
-          ratio: Math.max(...get(detailSection, 'images.backdrops', []).map(({ ratio }) => ratio)),
-          zoom: true
-        })
-      },
-      {
-        heading: labelImages.posters,
-        gridProps: (breakpoint) => ({
-          gap: '20px 10px',
-          itemsPerRow: itemsPerRowPoster(breakpoint)
-        }),
-        Element: MediaItemImage,
-        items: get(detailSection, 'images.posters', []),
-        props: (item) => ({
-          ...item,
-          ratio: Math.max(...get(detailSection, 'images.posters', []).map(({ ratio }) => ratio)),
-          zoom: true
-        })
-      }
-    ]
-  }
-});
