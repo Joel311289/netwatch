bc40455d2ed397870235847637be2f9ce1608ca0
Joel Paucarima <jopau3112@gmail.com>
2022-02-26 11:47:36 +0100

    Add media detail skeleton

--- src/components/Media/MediaDetail/MediaDetail-skeleton.jsx
+++ src/components/Media/MediaDetail/MediaDetail-skeleton.jsx
@@ -2,51 +2,91 @@ import PropTypes from 'prop-types';
 
 import { useBreakpointViewport } from '@hooks/useBreakpointViewport';
 
+import Slider from '@components/Layout/Slider/Slider';
 import Skeleton from '@components/UI/Skeleton/Skeleton';
 import Space from '@components/Layout/Space/Space';
-import MediaItemSkeleton from '@components/Media/MediaItem/MediaItem-skeleton';
 
 import { getEmptyArray } from '@utils/helpers/arrays';
+import { getWidthRatio } from '@utils/helpers/breakpoints';
 
 const MediaDetailSkeleton = ({ styles }) => {
-  const { sizeImage } = useBreakpointViewport();
+  const { mobile, smallDesktop } = useBreakpointViewport();
+  const imageWidth = () => {
+    return getWidthRatio(imageHeight(210), 1.5);
+  };
+  const imageHeight = (mobileHeight) => {
+    if (mobile) return mobileHeight;
+    if (smallDesktop) return 300;
+    return 375;
+  };
 
   return (
-    <div className={`${styles.wrapper} ${styles.skeleton}`}>
-      <div className={styles.background}>
-        <Skeleton height={300} />
-      </div>
-
+    <div className={styles.wrapper}>
       <div className={styles.content}>
-        <div className={styles.header}>
+        <div className={styles.header} style={{ marginTop: 20, marginBottom: 20 }}>
           <Skeleton width={280} height={30} />
           <Skeleton variant="text" width={180} height={15} style={{ marginTop: 5 }} />
         </div>
 
-        <div className={styles.image}>
-          <MediaItemSkeleton body={false} width={sizeImage} ratio={1.5} />
+        <div
+          className={styles.images}
+          style={{ gridTemplateColumns: mobile ? 'auto' : `${imageWidth()}px auto` }}>
+          <div className={styles.image} style={{ width: imageWidth(), height: imageHeight(210) }}>
+            <Skeleton height="100%" />
+          </div>
+
+          {!mobile && (
+            <div className={styles.background} style={{ width: '100%', height: imageHeight(210) }}>
+              <Skeleton height="100%" />
+            </div>
+          )}
         </div>
 
-        <Space gap={10} direction="column" className={`${styles.actions} ${styles.buttons}`}>
-          <Skeleton className={styles.button} height={44} />
-          <Skeleton className={styles.button} height={44} />
-        </Space>
-
-        <div className={styles.description}>
-          {getEmptyArray(4).map((_, index) => (
-            <Skeleton key={index} variant="text" style={{ maxWidth: '100%' }} />
+        <div className={`${styles.extras} ${styles.section}`}>
+          <div className={styles['section-heading']} style={{ marginTop: mobile ? 20 : 0 }}>
+            <Skeleton width={200} height={30} />
+          </div>
+          {getEmptyArray(2).map((_, index) => (
+            <Space key={index} gap={10} direction="column" className={styles['data-item']}>
+              <Skeleton height={25} width={150} />
+              <Skeleton height={20} width={250} />
+              <Skeleton height={20} width={250} />
+            </Space>
           ))}
+          <div style={{ marginTop: 25 }}>
+            <Space gap={10} className={`${styles.actions} ${styles.links}`}>
+              {getEmptyArray(5).map((_, index) => (
+                <Skeleton key={index} variant="circular" width={40} height={40} />
+              ))}
+            </Space>
+          </div>
         </div>
 
-        {getEmptyArray(1).map((_, index) => (
-          <div key={index} className={styles.section}>
+        <div className={`${styles.section} ${styles[`section-1`]}`}>
+          <div className={styles['section-heading']}>
+            <Skeleton width={200} height={30} />
+          </div>
+          <Space direction="column" gap={10}>
+            <Skeleton height={25} />
+            <Skeleton height={25} />
+            <Skeleton height={25} />
+          </Space>
+        </div>
+
+        {getEmptyArray(2).map((_, index) => (
+          <div key={index} className={`${styles.section} ${styles[`section-${index + 2}`]}`}>
             <div className={styles['section-heading']}>
               <Skeleton width={200} height={30} />
             </div>
-            <Space gap={10}>
-              <Skeleton height={150} width={250} />
-              <Skeleton height={150} width={250} />
-            </Space>
+
+            {!mobile && (
+              <Slider sliderPerView={mobile ? 1 : 2}>
+                <Skeleton height={200} />
+                <Skeleton height={200} />
+              </Slider>
+            )}
+
+            {mobile && <Skeleton height={200} />}
           </div>
         ))}
       </div>
